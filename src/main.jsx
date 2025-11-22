93682b09a2bc2917351a5f53e637190a28494451
Aidan Gaule <aidangaule@macbookair.mynetworksettings.com>
2025-11-22 10:50:16 -0500

    Fix:Update style

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-
-// Note: We are using the Tailwind CDN in index.html, so no local CSS import is needed here.
+import './index.css'; // This loads the compiled Tailwind styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
