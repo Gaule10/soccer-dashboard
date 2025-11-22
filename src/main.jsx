9ab37a22f1221cc046da53920e84af119a9b3fb1
Aidan Gaule <aidangaule@macbookair.mynetworksettings.com>
2025-11-22 10:21:23 -0500

    Fix / main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+// Note: We are using the Tailwind CDN in index.html, so no local CSS import is needed here.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
